510be2c040c516f71a674efa18e0638b7617e3c3
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-10-19 16:13:48 -0500

    i tried...would need to fix this

---
+++ Part9/Serialization/Games.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    public class Games
+    {
+        
+        public string name { get; set; }
+        public string platform { get; set; }
+        public string release_date { get; set; }
+        public string summary { get; set; }
+        public string meta_source { get; set; }
+        public string user_review { get; set; }
+
+        public Games()
+        {
+            name = "";
+            platform = "";
+            release_date="";
+            summary="";
+            meta_source = "";
+            user_review = "";
+                
+        }
+        public Games(string data)
+        {
+            var files = data.Split(',');
+            name = files[0];
+            platform = files[1];
+            release_date = files[2];
+            summary = files[3];
+            meta_source = files[4];
+            user_review = files[5];
+        }
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+    }
+}
--- Part9/Serialization/MainWindow.xaml.cs
+++ Part9/Serialization/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,46 @@ namespace Serialization
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Games> G = new List<Games>();
         public MainWindow()
         {
             InitializeComponent();
+
+            var lines = File.ReadAllLines("all_games.csv").Skip(1);
+            foreach (var item in lines)
+            {
+                G.Add(new Games(item));
+            }
+
+            foreach (var g in G)
+            {
+                lstbox.Items.Add(g);
+            }
+
+            PopulateCB(G);
+        }
+        private void PopulateCB(List<Games> gg)
+        {
+            cbBox.Items.Clear();
+            string[] value = Item.platform.Split(',');
+            foreach (var item in gg)
+            {
+                cbBox.Items.Add(item);
+            }
+
+        }
+            private void btb_Click(object sender, RoutedEventArgs e)
+            {
+
+                window w = new window();
+
+
+            }
+
+            private void cbBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+            {
+                Games selecteditem = (Games)cbBox.SelectedItem;
+            }
         }
     }
-}
+
